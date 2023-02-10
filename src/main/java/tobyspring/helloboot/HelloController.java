8e0560affa8fc1f4e0e6ce34f78c95c0b14687ea
sj <slaayer60@gmail.com>
2023-02-10 17:55:20 +0900

    front controller

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,11 +3,9 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
 
-    @GetMapping("/hello")
+public class HelloController {
     public String hello(String name){
-        return "hello" + name;
+        return "hello " + name;
     }
 }
