7e079e634b2aa21f70a2e16ac9bd4148e542adbf
sj <slaayer60@gmail.com>
2023-02-10 14:27:29 +0900

    init

---
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.helloboot;
+
+
+import org.apache.catalina.startup.Tomcat;
+
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+
+		new Tomcat().start();
+
+	}
+}
