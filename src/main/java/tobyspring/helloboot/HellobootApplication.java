979f199b6fe800cef89df753503987ecc4529382
sj <slaayer60@gmail.com>
2023-02-12 12:59:06 +0900

    dispatcher Servlet

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -5,10 +5,13 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,34 +22,18 @@ import java.io.PrintWriter;
 
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		ServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		WebServer webServer = factory.getWebServer(servletContext -> {
+    public static void main(String[] args) {
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.refresh();
 
-			HelloController helloController = new HelloController();
-
-
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안, 다국어, 공통
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-						String name = req.getParameter("name");
-
-						String ret = helloController.hello(name);
-
-						resp.setStatus(HttpStatus.OK.value());
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().print(ret);
-					}
-					else if(req.getRequestURI().equals("/user")){
-						// ...
-					}else{
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/*");
-		});
-		webServer.start();
-	}
+        ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        WebServer webServer = factory.getWebServer(servletContext -> {
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
+        });
+        webServer.start();
+    }
 }
